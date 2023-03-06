74377d539e468e46f5bab6dea66470b7c2766cca
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-06 20:08:35 +0600

    updated

---
+++ Programming_Basics/fahren.cpp
@@ -0,0 +1,18 @@
+// Fahrenheit to Celcius
+
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ftemp;  // for temperature in fahrenheit
+    
+    cout << "Enter temperature in Fahrenheit: ";
+    cin >> ftemp;
+    
+    int ctemp = (ftemp - 32) * 5 / 9;
+    cout << "Equivalent in Celsius is: " << ctemp << '\n';
+
+    return 0;
+}
---
+++ importantNotes.md
@@ -0,0 +1,14 @@
+# Important Notes on C++
+
+### Escape Sequence
+Common Escape Sequences:-
+- \a Bell
+- \b Backspace
+- \f Formfeed
+- \n NewLine
+- \r Return
+- \t Tab
+- \ \ Backslash
+- \' Single quotation mark
+- \" Double quotation marks
+\ xdd HExadecimal notation
