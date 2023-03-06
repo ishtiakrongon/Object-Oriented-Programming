ce52e84044845eb84fbd6c19ffd3261fbb3d5955
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-06 19:36:49 +0600

    updated

--- Programming_Basics/beginning.cpp
+++ Programming_Basics/beginning.cpp
@@ -6,5 +6,6 @@ using namespace std;
 
 int main()
 {
-    
+    cout << "Hello Darkness! my old friend! This time I come back all alone\n";
+    return 0;
 }
---
+++ Programming_Basics/charvars.cpp
@@ -0,0 +1,15 @@
+// demonstrates character variables
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char charVar1 = 'H';
+    char charVar2 = 'B';
+
+    cout << charVar1 << endl;
+    cout << charVar2 << endl;
+    return 0;
+}
